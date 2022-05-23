87a8eab13894c4bedc1c09c00c202f7e839a8ab3
bdubs-astro <bwissman@k-space.com>
2022-05-23 18:00:47 -0400

    Move function readTxtFile to a separate m file.

--- NumInt.m
+++ NumInt.m
@@ -1,5 +1,7 @@
-% Numerical Integration utilizing the Trapezoidal Rule 
+% Numerical Integration utilizing the Trapezoidal Rule
+%
 % https://www.mathworks.com/help/matlab/ref/trapz.html
+% https://web.engr.oregonstate.edu/~webbky/MAE4020_5020_files/Section%208%20Integration.pdf
 
 %% default parameters
 lowerLim = 903.5;
@@ -82,40 +84,3 @@ hh(1).LineStyle = '--';  hh(1).LineWidth = 1.0;  hh(1).Color = 'r';
 hh(2).LineStyle = '--';  hh(2).LineWidth = 1.0;  hh(2).Color = 'r';
 title(sprintf('limits: %0.2f - %0.2f, dx = %0.2f, Sum*dx = %.6G, Trapz = %.6G', min(xInt), max(xInt), dx, mySum, myTrap))
 plotbrowser 'on'
-
-%% function(s)
-
-function [inpPathname, inpFilename, header, xRaw, dx, yRaw] = readTxtFile(inputDir, msg)
-% Reads a txt file and returns the 1st two data columns as doubles
-
-    % read txt file with or w/o header ...
-    [inpFilename, inpPathname] = uigetfile('*.txt', msg, inputDir);
-    if isequal(inpFilename, 0)                          % user cancelled operation
-        error('Error: File open cancelled by user ... halting execution.');
-    end
-
-    [fileID,errmsg] = fopen(fullfile(inpPathname, inpFilename));
-    if ~isempty(errmsg)
-        error('Error: File open error ... halting execution.');
-    end
-
-    % check for header ...
-    line = fgetl(fileID); headertest = sscanf(line, '%s');
-    if (headertest(1) == '#')
-        header = strtrim(line(2:end));  % remove '#' as well as leading and trailing white-space characters     
-    else
-        header = ' ';
-    end
-    data_in = readmatrix(fullfile(inpPathname, inpFilename), 'OutputType', 'double', 'FileType', 'text');
-    fclose('all');
-
-    xRaw = data_in(:,1);
-    dx = ( max(xRaw) - min(xRaw) )/( length(xRaw) - 1 );    % x-spacing
-    yRaw = data_in(:,2);
-    
-    % confirm that data is valid by comparing the number of non-zero values
-    if isempty(xRaw) || length(xRaw) ~= length(yRaw) 
-        error('Error: Missing or invalid input data ... halting execution.');
-    end
-    
-end
